576fe17bbbdc332d9187151ebfaba3303733741d
JordyPazaran <jordyxavierfi@gmail.com>
2017-09-11 00:09:09 -0500

    Update rep.sh

--- rep.sh
+++ rep.sh
@@ -2,9 +2,9 @@
 
 printf "\033c"		#limpia pantalla
 while [ "$op" != "6" ];do
-RESPUESTA=$(dpkg --get-selections | grep -w mpg123 | grep -w install)
-  if [ "$RESPUESTA" = "" ]; then
-      echo -e " El mpg123 no esta instalado. Se porcedará a instalar"
+MPG=$(dpkg --get-selections | grep -w mpg123 | grep -w install)
+  if [ "$MPG" = "" ]; then
+      echo -e " Mpg123 no esta instalado. Se porcedará a instalar"
   
 		sudo apt-get install mpg123
 		echo -e "\n El mpg123 esta instalado."
@@ -12,22 +12,22 @@ RESPUESTA=$(dpkg --get-selections | grep -w mpg123 | grep -w install)
   else
      
  	  echo  "\n"
-	  echo -e "\E[1;37m  Carpeta actual: `pwd`\n"
-	  echo  "Seleccione la opción que desea realizar: \n "
-	  echo  ")Reproducir el contenido de la carpeta actual"
-	  echo  ")Cambiar de carpeta"
-	  echo  ")Subir una carpeta"
-	  echo  ")Listar archivos y subcarpetas de la carpeta actual"
-	  echo  ")Listar opciones del reproductor"
-	  echo  ")Salir"
+	  echo -e "\E[1;37m  Carpeta actual: `pwd`\n" #nos muestra dirección 
+	  echo  "Selecciona la opción a realizar: \n "
+	  echo  "1)Reproducir el contenido "
+	  echo  "2)Cambiar de carpeta"
+	  echo  "3)Subir una carpeta"
+	  echo  "4)LArból de las carpetas"
+	  echo  "5)Listar opciones del reproductor"
+	  echo  "6)Salir"
 	  
 	  read op
 	  
-	  toca= pwd
+	  get= pwd
 	  case $op in
 		  1)
 		   echo  "[	[s] Pausa\n[f] Siguiente\n[d] Anterior\n[b] Repetir\n[.] Adelante\n[,] Atrás\n[+]Volumen Arriba\n[-] Volumen Abajo\n[q] Quitar reproducción\n[h] Listar más opciones del mpg123\n "
-	 	  `mpg123 -C -d $toca* `
+	 	  `mpg123 -C -d $get* ` #se ejecunatn comando de mpg123
 	 	  clear
 	 	  ;;
 		  2) 
@@ -43,22 +43,21 @@ RESPUESTA=$(dpkg --get-selections | grep -w mpg123 | grep -w install)
 		  ;;
 		  4) 
 		  printf "\033c"		
-		  /bin/bash ./generales.sh arbol $cdo
-		  echo -e "\E[1;35m	\nPresione una tecla para continuar..."
+		  /bin/bash ./arbol.sh $pwd
+		  echo -e "\E[1;35m	\nPresione cauqluier tecla"
 		  read
 		  clear
 		  ;;
 		  5)
-		  echo -e "\E[1;33m	\nEstas son las opciones del PrebePlayer: \n[s] Pausa\n[f] Siguiente\n[d] Anterior\n[b] Repetir\n[.] Adelante\n[,] Atrás\n[+]Volumen Arriba\n[-] Volumen Abajo\n[q] Quitar reproducción\n[h] Listar más opciones del mpg123\n "  
-		  echo -e "\E[1;35m	\nPresione una tecla para continuar..."
+		  echo -e "\E[1;33m	\nEOpciones: \n[s] Pausa\n[f] Siguiente\n[d] Anterior\n[b] Repetir\n[.] Adelante\n[,] Atrás\n[+]Volumen Arriba\n[-] Volumen Abajo\n[q] Quitar reproducción\n[h] Listar más opciones del mpg123\n "  
+		  echo -e "\E[1;31m	\nPresiona cualquier tecla"
 		  read
 		  clear
 		  ;;
 		  6)
-			echo -e "\E[1;32m	¡HASTA LUEGO!"
 		  ;;
-		  *) echo -e "\E[1;31m	Opcion inválida. Intente de nuevo"
-		  echo -e "Presione una tecla para continuar..."
+		  *) echo -e "\E[1;31m	Opcion  incorecta"
+		  echo -e "Presione cualquier tecla"
 		  read
 		  clear
 		  ;;
